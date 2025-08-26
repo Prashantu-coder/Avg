9c3a60e61f86cfb6280205593ab0943a29da43da
Prashantu-coder <prashantstha0912@gmail.com>
2025-08-26 20:28:06 +0545

    Add files via upload

--- Average.py
+++ Average.py
@@ -8,14 +8,3 @@ def load_google_sheet(sheet_id, gid):
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv&gid={gid}"
     df= pd.read_csv(url)
     return df
-
-data = load_google_sheet(sheet_id, gid)
-
-
-html_table = data.to_html(index=False)
-
-with open("sheet_data.html", "w", encoding="utf-8") as f:
-    f.write("<html><head><title>Google Sheet Data</title></head><body>")
-    f.write("<h2>Imported Google Sheet</h2>")
-    f.write(html_table)
-    f.write("</body></html>")
